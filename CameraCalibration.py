1a414ac4ea5691bde2ef18d0292197c5716fcfd0
Joachim <60391253+joantfolk@users.noreply.github.com>
2020-11-17 20:11:11 +0100

    en enkel version av kamera kalibreringen
    Klassen har funktionalitet för att läsa och skriva till en .ini-fil samt för att kalibrera kameran utifrån bredden och höjden av ett objekt i bilden

---
+++ CameraCalibration.py
@@ -0,0 +1,56 @@
+class CameraCalibration:
+
+    def __init__(self, width, height, distance):
+        """
+        Initiates CameraCalibration object with real dimension integer references 'width' and 'height' 
+        """
+        self.width = width
+        self.height = height
+        self.distance = distance
+
+    def write_to_file(self, fx: int, fy: int):
+        """
+        Writes the integer parameters fx and fy to camera_info.ini  
+        otherwise throws exception 
+        """
+        try:
+            file = open("camera_info.ini", "w")
+            file.write("fx:" + str(fx) + "\nfy:" + str(fy))
+        except Exception:
+            raise Exception("Could not write to file!")
+        finally:
+            file.close()
+        
+        
+
+    def read_from_file(self) -> (int, int):
+        """ 
+        Reads the relevant camera inforamtion from camera_info.ini 
+        returns a tuple in the format (fx, fy) 
+        otherwise throws exception 
+        """
+        try:
+            file = open("camera_info.ini", "r")
+            fx = int(file.readline().split(":")[1])
+            fy = int(file.readline().split(":")[1])
+            retval = (fx, fy)
+        except Exception:
+            raise Exception("Could not read from file!")
+        finally:
+            file.close()
+            return retval
+        
+
+    def calibrate(self, imgwidth: int, imgheight: int) -> (int, int):
+        """
+        Takes the width and height of the recognised object in pixels 
+        and calculates the focal length using the formula: 
+        Focal length = (Size in image x Distance) / Real size
+        """
+        fx = (imgwidth * self.distance) / self.width
+        fy = (imgheight * self.distance) / self.height
+        try:
+            self.write_to_file(fx, fy)
+        except Exception:
+            raise Exception("Could not write to file!")
+        return (fx, fy)
