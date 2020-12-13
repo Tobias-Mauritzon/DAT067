72dfc1e95a74dd5d1dab938297c4cdcaee79b5a4
Philip <60391080+phiaxen@users.noreply.github.com>
2020-12-13 12:08:11 +0100

    added comments on yolo and haarcascade classes

--- Yolov3_Model.py
+++ Yolov3_Model.py
@@ -1,22 +1,26 @@
 import cv2
 import numpy as np
 
+# Author: Philip
+# Reviewed by:
+# Date: 2020-12-13
+
+"""This class loads loads the yolo method for object detection and uses it to find 80 different objects with the function findObject"""
 class Yolo_Model():
     def __init__(self):
         self.ObjectsFile = 'Yolo_v3_tiny/coco.names'
         self.objectNames = []
         self.CONFTRESHOLD = 0.5
-        self.nms_threshold = 0.3
+        self.nms_threshold = 0.35
         self.__loadObjectNames()
         self.__readNet()
 
+    # Read file with object names
     def __loadObjectNames(self):
         with open(self.ObjectsFile,'rt') as f:
             self.objectNames = f.read().rstrip('\n').split('\n')
-        #print(self.ObjectNames)
-        #print(len(self.ObjectNames))
     
-    #read the network
+    # Read the network
     def __readNet(self):
         self.weights = 'Yolo_v3_tiny/yolov3-tiny.weights'
         self.configFile = 'Yolo_v3_tiny/yolov3-tiny.cfg'
@@ -24,10 +28,11 @@ class Yolo_Model():
         self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
         self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
     
-    # convert to blobformat since the network only knows that format
+    # Convert to blobformat since the network only knows that format
     def __prepare(self, image):
         self.blob = cv2.dnn.blobFromImage(image,1/255,(250,250),[0,0,0],1,crop=False)
 
+    # This function is used in the findObjects function. It loops through each detection in each output and prints out text and bounding boxes for each object.
     def __find(self, outputs, image):
         imgHeight, imgWidth, imgChannels = image.shape
         boundingBox = []
@@ -55,16 +60,13 @@ class Yolo_Model():
             cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,255),2)
             cv2.putText(image,f'{self.objectNames[classIds[i]].upper()} {int(confidenceValues[i]*100)}%', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,255), 2)
     
+    # Finds objects 
     def findObjects(self, image):
         self.__prepare(image)
         self.net.setInput(self.blob)
-
         layerNames = self.net.getLayerNames()
-
         outputNames = [layerNames[i[0]-1] for i in self.net.getUnconnectedOutLayers()]
-
         outPuts = self.net.forward(outputNames)
-
         self.__find(outPuts,image)
 
         
